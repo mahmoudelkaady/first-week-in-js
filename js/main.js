16ae41e2ce1e9b33dfa44caa1be77018a16a54fb
mahmoud medhat <mahmodmht.8.8@gmail.com>
2023-04-17 07:26:31 +0200

    this is my first week in js

---
+++ js/main.js
@@ -0,0 +1,10 @@
+document.querySelector("h1").style.color = "blue";
+/* window.alert("hollw wotld");
+// its not the best prabtice to do an alert message */
+// document.write("<h1>hellow world</h1>");
+//
+/* console.log("log");
+// console.error("error");
+// console.table(["medhat", "osama", "ahmed"]);
+// console.log("welcome from %cjs file", "color:red; font-size:40px;");
+*/
